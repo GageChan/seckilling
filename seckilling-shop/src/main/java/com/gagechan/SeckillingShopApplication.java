f62ae359d39f180ad5e745932376ac12447b99c5
gagechan <gagechan@foxmail.com>
2018-07-19 11:50:53 +0800

    init project

---
+++ seckilling-shop/src/main/java/com/gagechan/SeckillingShopApplication.java
@@ -0,0 +1,12 @@
+package com.gagechan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillingShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeckillingShopApplication.class, args);
+	}
+}
---
+++ seckilling-shop/src/main/java/com/gagechan/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.gagechan.controller;
+
+/**
+ * @program: seckilling
+ *
+ * @author: GageChan
+ *
+ * @create: 2018年07月19日 11:49
+ *
+ * @description:
+ **/
+public class IndexController {
+}
