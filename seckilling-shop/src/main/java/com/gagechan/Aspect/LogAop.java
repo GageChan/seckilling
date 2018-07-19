d92304afcd076689659f4ea984b9cc6f8f674bf9
gagechan <gagechan@foxmail.com>
2018-07-19 13:09:10 +0800

    create project structures and add log for controllers

---
+++ seckilling-shop/src/main/java/com/gagechan/Aspect/LogAop.java
@@ -0,0 +1,56 @@
+package com.gagechan.Aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+/**
+ * @program: seckilling
+ *
+ * @author: GageChan
+ *
+ * @create: 2018年07月19日 12:48
+ *
+ * @description: 记录日志
+ **/
+
+@Slf4j
+@Component
+@Aspect
+public class LogAop {
+	@Pointcut("execution(public * com.gagechan.controller.*Controller.*(..))")
+	public void controller(){
+	}
+
+	@Before("controller()")
+	public void controllerBefor(JoinPoint point){
+		log.info("---------begin---------");
+		ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+		HttpServletRequest request = servletRequestAttributes.getRequest();
+
+		log.info("URL:{}",request.getRequestURL());
+		log.info("HTTP_METHOD:{}",request.getMethod());
+		log.info("IP:{}",request.getRemoteAddr());
+		log.info("CLASS_METHOD:{}.{}",point.getSignature().getDeclaringTypeName(),point.getSignature().getName());
+		log.info("parameters:");
+		Enumeration<String> enu = request.getParameterNames();
+		while (enu.hasMoreElements()) {
+			String paraName = enu.nextElement();
+			log.info("{}:{}",paraName,request.getParameter(paraName));
+		}
+	}
+
+	@After("controller()")
+	public void controllerAfter(){
+		log.info("---------end------------");
+	}
+}
--- seckilling-shop/src/main/java/com/gagechan/controller/IndexController.java
+++ seckilling-shop/src/main/java/com/gagechan/controller/IndexController.java
@@ -1,5 +1,8 @@
 package com.gagechan.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @program: seckilling
  *
@@ -9,5 +12,10 @@ package com.gagechan.controller;
  *
  * @description:
  **/
+@Controller
 public class IndexController {
+	@RequestMapping("hello")
+	public String index(){
+		return "hello";
+	}
 }
